e2dfe184eec967282561f15285709ab84051c0ae
93mcqmil <mcqueen_1993@hotmail.com>
2024-05-02 15:38:14 +0200

    comments on useRouter to HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,7 +3,10 @@ import Navbar from "./Navbar/Navbar";
 import Books from "./Navbar/Books";
 import Favorites from "./Navbar/Favorites";
 
+// ensuring HomePage component remains mounted
+// when navigating to the routes
 const HomePage: React.FC = () => {
+  // Hook to match current url path with defined routes
   const routes = useRoutes([
     { path: "/books", element: <Books /> },
     { path: "/favorites", element: <Favorites /> },
@@ -16,3 +19,6 @@ const HomePage: React.FC = () => {
   );
 };
 export default HomePage;
+
+//useRouter provides information about current URL. (allowing components to react to changes in the URL).
+// handling navigation
