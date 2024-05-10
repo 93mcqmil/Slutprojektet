a6a5deed0c4a6b9006f40964f18aa68302475821
93mcqmil <mcqueen_1993@hotmail.com>
2024-05-10 10:34:16 +0200

    User can search for book and Author

--- src/Globalstate.tsx
+++ src/Globalstate.tsx
@@ -1,12 +1,13 @@
-import { Key, ReactNode, SetStateAction, createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import Books from "./components/Navbar/Books";
 
 // define type for global state
-type GlobalState = {
+export type GlobalState = {
   bookResults: BookResult[];
   favorites: favoriteArray[];
   authorResults: authorResult[];
   updateSearchResultsBooks: (newBooks: BookResult[]) => void; //add the update function here
+  updateSearchResultsAuthors: (newAuthors: authorResult[]) => void;
 };
 /*********************************** */
 export interface RouterError {
@@ -20,6 +21,7 @@ export interface BookResult {
   author_name: string;
   title: string;
   ebook_access: string;
+  first_publish_year: number;
 }
 export interface BookSearchResult {
   numFound: number;
@@ -29,59 +31,56 @@ export interface BookSearchResult {
 }
 /******************************* */
 export interface authorResult {
+  type: string;
+  top_work: string;
+  key: string;
+  birth_date: number;
   author_key: number;
   author_name: string;
-  // type: ReactNode;
-  // top_work: ReactNode;
-  // birth_date: ReactNode;
-  // key: Key | null | undefined;
-  // author_name: string;
-  // numfound: number;
-  // start: number;
-  // numFoundExact: boolean;
-  // docs: {
-  //   key: string;
-  //   name: string;
-  //   top_subjects: string[];
-  //   top_work: string;
-  //   type: string;
-  //   work_count: number;
-  // }[];
+}
+export interface authorSearchResult {
+  numFound: number;
+  start: number;
+  numFoundExact: boolean;
+  docs: authorResult[];
 }
 
-// create global state context and we keep track of our books
+// create global state context to keep track of books
 export const GlobalStateContext = createContext<GlobalState>({
   bookResults: [], // array of books from api
   favorites: [], // array of favorite books
   authorResults: [],
-  updateSearchResultsBooks: function (newBooks: BookResult[]): void {
-    throw new Error("Function not implemented.");
-  },
+  updateSearchResultsBooks: () => {},
+  updateSearchResultsAuthors: () => {},
 });
 
 //create global state provider component
 export const GlobalStateProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [searchResultsBooks, setsearchResultsBooks] = useState<BookResult[]>(
-    []
-  );
-  const [authorResults, setAuthorsResults] = useState<authorResult[]>([]); // initialize state variable
+  const [bookResults, setBookResults] = useState<BookResult[]>([]); // state variable for book results
+  const [authorResults, setAuthorsResults] = useState<authorResult[]>([]); // state variable for author results
 
+  // update function for book results
   const updateSearchResultsBooks = (newBooks: BookResult[]) => {
-    setsearchResultsBooks(newBooks);
+    setBookResults(newBooks);
+  };
+  // update function for author results
+  const updateSearchResultsAuthors = (newAuthors: authorResult[]) => {
+    setAuthorsResults(newAuthors);
   };
 
-  //create global state provider component
+  const value: GlobalState = {
+    bookResults,
+    favorites: [],
+    authorResults,
+    updateSearchResultsBooks,
+    updateSearchResultsAuthors,
+  };
+
+  // Create global state provider component
   return (
-    <GlobalStateContext.Provider
-      value={{
-        bookResults: searchResultsBooks,
-        favorites: [],
-        authorResults,
-        updateSearchResultsBooks,
-      }}
-    >
+    <GlobalStateContext.Provider value={value}>
       {children}
     </GlobalStateContext.Provider>
   );
--- src/components/Navbar/SearchField/SearchField.tsx
+++ src/components/Navbar/SearchField/SearchField.tsx
@@ -1,129 +1,70 @@
-// import { ChangeEvent, useState } from "react";
-// import axios from "axios";
-// import { bookResult, authorResult } from "../../../Globalstate";
-
-// export default function SearchField() {
-//   const [searchTerm, setSearchTerm] = useState("");
-//   const [searchResultsAuthors, setSearchResultsAuthors] = useState<
-//     authorResult[] | null
-//   >(null);
-//   const [searchResultsBooks, setSearchResultsBooks] = useState<
-//     bookResult[] | null
-//   >(null);
-//   const [loading, setLoading] = useState(false);
-
-//   const handleSearch = async () => {
-//     if (searchTerm.trim() === "") {
-//       // Empty search term, do nothing
-//       return;
-//     }
-
-//     const lowercaseSearchTerm = searchTerm.toLowerCase(); // convert to lowercase
-
-//     try {
-//       setLoading(true);
-
-//       const responseAuthors = await axios.get<authorResult>(
-//         `https://openlibrary.org/search/authors.json?q=${lowercaseSearchTerm}&limit=1`
-//       );
-
-//       setSearchResultsAuthors(responseAuthors.data.docs);
-
-//       const responseBooks = await axios.get<bookResult>(
-//         `https://openlibrary.org/search.json?title=${lowercaseSearchTerm}&limit=1`
-//       );
-//       setSearchResultsBooks(responseBooks.data.docs);
-//       console.log("Book Data: ", responseBooks.data.docs);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//       setSearchResultsAuthors([]);
-//       setSearchResultsBooks([]);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setSearchTerm(event.target.value);
-//   };
-
-//   return (
-//     <>
-//       <label htmlFor='site-search'>Search the site:</label>
-//       <input
-//         type='search'
-//         id='site-search'
-//         name='q'
-//         value={searchTerm}
-//         onChange={handleChange}
-//       />
-
-//       <button onClick={handleSearch}>Search</button>
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <>
-//           {searchResultsAuthors && searchResultsAuthors.length > 0 && (
-//             <div>
-//               <h2>Authors</h2>
-//               <ul>
-//                 {searchResultsAuthors.map((author) => (
-//                   <li key={author.key}>
-//                     <div>Name: {author.name}</div>
-//                     <div>Birth Date: {author.birth_date}</div>
-//                     <div>Top Work: {author.top_work}</div>
-//                     <div>Type: {author.type}</div>
-//                   </li>
-//                 ))}
-//               </ul>
-//             </div>
-//           )}
-
-//           {searchResultsBooks && searchResultsBooks.length > 0 && (
-//             <div>
-//               <h2>Books</h2>
-//               <ul>
-//                 {searchResultsBooks.map((book) => (
-//                   <li key={book.key}>{book.title}</li>
-//                 ))}
-//               </ul>
-//             </div>
-//           )}
-//         </>
-//       )}
-//     </>
-//   );
-// }
-import React, { ChangeEvent, useState } from "react";
-import { useDataFetching } from "../../../hooks/useDataFetching"; // Assuming useDataFetching is in a separate file
-import { authorResult, BookResult } from "../../../Globalstate";
+import React, { ChangeEvent, useContext, useEffect, useState } from "react";
+import {
+  authorResult,
+  authorSearchResult,
+  BookResult,
+  BookSearchResult,
+} from "../../../Globalstate";
+import { GlobalStateContext } from "../../../Globalstate";
 
 export default function SearchField() {
   const [searchTerm, setSearchTerm] = useState("");
   const [loading, setLoading] = useState(false);
+  const [searchResultsAuthors, setSearchResultsAuthors] =
+    useState<authorSearchResult | null>(null);
+  const [searchResultsBooks, setSearchResultsBooks] =
+    useState<BookSearchResult | null>(null);
+  const { updateSearchResultsBooks } = useContext(GlobalStateContext);
+  const { updateSearchResultsAuthors } = useContext(GlobalStateContext);
+  const [searchClicked, setSearchClicked] = useState(false);
 
-  // Using the custom hook to fetch data for authors and books
-  const searchResultsAuthors = useDataFetching<authorResult[] | null>(
-    searchTerm
-      ? `https://openlibrary.org/search/authors.json?q=${searchTerm}&limit=5`
-      : ""
-  );
-  const searchResultsBooks = useDataFetching<BookResult[] | null>(
-    searchTerm
-      ? `https://openlibrary.org/search.json?title=${searchTerm}&limit=5`
-      : ""
-  );
-  console.log("Authors Data: ", searchResultsAuthors.data);
-  console.log("Books Data: ", searchResultsBooks.data);
+  useEffect(() => {
+    console.log("useEffect triggered");
+    const fetchData = async () => {
+      if (!searchClicked || searchTerm.trim() === "") {
+        return;
+      }
 
-  const handleSearch = async () => {
-    if (searchTerm.trim() === "") {
-      return;
-    }
+      setLoading(true);
 
-    setLoading(true);
-    // No need for manual fetching here, handled by useDataFetching hook
-    setLoading(false);
+      try {
+        const authorSearchAPI = `https://openlibrary.org/search/authors.json?q=${searchTerm}&limit=1`;
+        const bookSearchAPI = `https://openlibrary.org/search.json?title=${searchTerm}&limit=1`;
+
+        const authorResponse = await fetch(authorSearchAPI);
+        const bookResponse = await fetch(bookSearchAPI);
+
+        if (!authorResponse.ok || !bookResponse.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const authorSearchData: authorSearchResult =
+          await authorResponse.json();
+        const bookSearchData: BookSearchResult = await bookResponse.json();
+
+        setSearchResultsAuthors(authorSearchData);
+        console.log("Authors Data:", authorSearchData.docs);
+
+        setSearchResultsBooks(bookSearchData);
+        console.log("Books Data:", bookSearchData);
+        updateSearchResultsBooks(bookSearchData.docs);
+      } catch (error) {
+        console.error("Error fetching search results:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [
+    searchClicked,
+    searchTerm,
+    updateSearchResultsBooks,
+    updateSearchResultsAuthors,
+  ]);
+
+  const handleSearch = () => {
+    setSearchClicked(true);
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -147,14 +88,14 @@ export default function SearchField() {
       ) : (
         <>
           {searchResultsAuthors &&
-            Array.isArray(searchResultsAuthors) &&
-            searchResultsAuthors.length > 0 && (
+            searchResultsAuthors.docs &&
+            searchResultsAuthors.docs.length > 0 && (
               <div>
                 <h2>Authors</h2>
                 <ul>
-                  {searchResultsAuthors.map((author: any) => (
+                  {searchResultsAuthors.docs.map((author: authorResult) => (
                     <li key={author.key}>
-                      <div>Name: {author.name}</div>
+                      <div>Name: {author.author_name}</div>
                       <div>Birth Date: {author.birth_date}</div>
                       <div>Top Work: {author.top_work}</div>
                       <div>Type: {author.type}</div>
@@ -165,17 +106,18 @@ export default function SearchField() {
             )}
 
           {searchResultsBooks &&
-            searchResultsBooks.data &&
-            searchResultsBooks.data.docs && (
+            searchResultsBooks.docs &&
+            searchResultsBooks.docs.length > 0 && (
               <div>
                 <h2>Books</h2>
                 <ul>
-                  {searchResultsBooks.data.docs.map((book: BookResult) => (
+                  {searchResultsBooks.docs.map((book: BookResult) => (
                     <li key={book.key}>
                       <div>Title: {book.title}</div>
                       <div>Author key: {book.author_key}</div>
-                      <div>Name: {book.author_name}</div>
+                      {/* <div>Name: {book.author_name}</div> */}
                       <div>Ebook: {book.ebook_access}</div>
+                      <div>First publish year: {book.first_publish_year}</div>
                     </li>
                   ))}
                 </ul>
