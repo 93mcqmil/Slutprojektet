9cdc95e2b2f543fee0f77d0fefa046c481730f03
93mcqmil <mcqueen_1993@hotmail.com>
2024-05-19 14:58:16 +0200

    changed routes so that searchfield is only rendered in the hompage. not when navigating elsewhere

--- src/components/Homepage/HomePage.tsx
+++ src/components/Homepage/HomePage.tsx
@@ -2,30 +2,50 @@ import { useRoutes } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Books from "../Navbar/Books";
 import Favorites from "../Navbar/Favorites";
-import SearchField from "../Navbar/SearchField/SearchField";
+
 import { GlobalStateProvider } from "../../Globalstate";
 import "tailwindcss/tailwind.css";
+import { HomePageContent } from "./HomePageContent";
 
-// ensuring HomePage component remains mounted
-// when navigating to the routes
+// HomePage component
 const HomePage: React.FC = () => {
   // Hook to match current url path with defined routes
   const routes = useRoutes([
+    { path: "/", element: <HomePageContent /> },
     { path: "/books", element: <Books /> },
     { path: "/favorites", element: <Favorites /> },
   ]);
+
   return (
     <>
-      {" "}
       <GlobalStateProvider>
         <Navbar />
-        <SearchField />
         <main>{routes}</main>
       </GlobalStateProvider>
     </>
   );
 };
 export default HomePage;
+// // ensuring HomePage component remains mounted
+// // when navigating to the routes
+// const HomePage: React.FC = () => {
+//   // Hook to match current url path with defined routes
+//   const routes = useRoutes([
+//     { path: "/books", element: <Books /> },
+//     { path: "/favorites", element: <Favorites /> },
+//   ]);
+//   return (
+//     <>
+//       {" "}
+//       <GlobalStateProvider>
+//         <Navbar />
+//         <SearchField />
+//         <main>{routes}</main>
+//       </GlobalStateProvider>
+//     </>
+//   );
+// };
+// export default HomePage;
 
 //useRouter provides information about current URL. (allowing components to react to changes in the URL).
 // handling navigation
---
+++ src/components/Homepage/HomePageContent.tsx
@@ -0,0 +1,18 @@
+import { useLocation } from "react-router-dom";
+import SearchField from "../Navbar/SearchField/SearchField";
+
+// HomePageContent component
+export const HomePageContent: React.FC = () => {
+  const { pathname } = useLocation();
+
+  // Render the SearchField only if the current path is the homepage
+  if (pathname === "/") {
+    return (
+      <div>
+        <SearchField />
+      </div>
+    );
+  } else {
+    return null; // Render nothing for other paths
+  }
+};
