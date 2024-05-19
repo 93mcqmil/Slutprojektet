ccb389366910104a6cd2dcfb3fe7c4eb65f0c92b
93mcqmil <mcqueen_1993@hotmail.com>
2024-05-19 21:04:36 +0200

    display flex on the favorites component

--- src/components/Navbar/Favorites.tsx
+++ src/components/Navbar/Favorites.tsx
@@ -1,3 +1,48 @@
+// import React, { useContext } from "react";
+// import {
+//   GlobalStateContext,
+//   BookResult,
+//   authorResult,
+// } from "../../Globalstate";
+// import BookCardFavorites from "../Card/CardBookFavorites.tsx";
+// import AuthorCardFavorites from "../Card/CardAuthorFavorite.tsx";
+
+// export default function Favorites() {
+//   const { favorites, removeFromFavorites } = useContext(GlobalStateContext);
+
+//   return (
+//     <div>
+//       <h1>Favorite Items</h1>
+//       <ul>
+//         {favorites.map((item) => (
+//           <li key={isBookResult(item) ? item.key : (item as authorResult).key}>
+//             {isBookResult(item) ? (
+//               <div key={item.key} className='p-2 w-full md:w-1/2 lg:w-1/3'>
+//                 <BookCardFavorites
+//                   book={item}
+//                   onRemoveFromFavorites={removeFromFavorites}
+//                 />
+//               </div>
+//             ) : (
+//               <div key={item.key} className='p-2 w-full md:w-1/2 lg:w-1/3'>
+//                 <AuthorCardFavorites
+//                   author={item}
+//                   onRemoveFromFavorites={removeFromFavorites}
+//                 />
+//               </div>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+
+//   // Define a type guard function to check if an item is a BookResult/and accepts objects of type bookresults or authorresults
+//   function isBookResult(item: BookResult | authorResult): item is BookResult {
+//     return "title" in item;
+//   }
+// }
+
 import React, { useContext } from "react";
 import {
   GlobalStateContext,
@@ -12,32 +57,31 @@ export default function Favorites() {
 
   return (
     <div>
-      <h1>Favorite Items</h1>
-      <ul>
+      <h1 className='text-2xl font-bold mb-4'>Favorite Items</h1>
+      <div className='flex flex-wrap -mx-2'>
         {favorites.map((item) => (
-          <li key={isBookResult(item) ? item.key : (item as authorResult).key}>
+          <div
+            key={isBookResult(item) ? item.key : (item as authorResult).key}
+            className='p-2 w-full md:w-1/2 lg:w-1/3'
+          >
             {isBookResult(item) ? (
-              <div key={item.key} className='p-2 w-full md:w-1/2 lg:w-1/3'>
-                <BookCardFavorites
-                  book={item}
-                  onRemoveFromFavorites={removeFromFavorites}
-                />
-              </div>
+              <BookCardFavorites
+                book={item}
+                onRemoveFromFavorites={removeFromFavorites}
+              />
             ) : (
-              <div key={item.key} className='p-2 w-full md:w-1/2 lg:w-1/3'>
-                <AuthorCardFavorites
-                  author={item}
-                  onRemoveFromFavorites={removeFromFavorites}
-                />
-              </div>
+              <AuthorCardFavorites
+                author={item}
+                onRemoveFromFavorites={removeFromFavorites}
+              />
             )}
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 
-  // Define a type guard function to check if an item is a BookResult/and accepts objects of type bookresults or authorresults
+  // Define a type guard function to check if an item is a BookResult
   function isBookResult(item: BookResult | authorResult): item is BookResult {
     return "title" in item;
   }
