3d7ec5033b3cf630610e6e6eb48199660708187b
93mcqmil <mcqueen_1993@hotmail.com>
2024-05-23 09:43:27 +0200

    Styling to the BookCard component

--- src/components/Card/CardBookSearch.tsx
+++ src/components/Card/CardBookSearch.tsx
@@ -16,7 +16,7 @@ const BookCardSearch: React.FC<BookCardSearchProps> = ({
 }) => {
   const [truncateText, setTruncate] = useState(true);
   const [isAddedToFavorites, setIsAddedToFavorites] = useState(false);
-  const [isAddedtoReadBooks, setIsAdded] = useState(false);
+  const [isAddedToReadBooks, setIsAddedToReadBooks] = useState(false);
   const [reviewText, setReviewText] = useState("");
   const [submittedReview, setSubmittedReview] = useState<string[]>([]);
 
@@ -27,7 +27,7 @@ const BookCardSearch: React.FC<BookCardSearchProps> = ({
 
   const handleAddToReadBooks = () => {
     onAddToReadBooks(book);
-    setIsAdded(true);
+    setIsAddedToReadBooks(true);
   };
 
   const handleReviewSubmit = (event: React.FormEvent) => {
@@ -39,68 +39,64 @@ const BookCardSearch: React.FC<BookCardSearchProps> = ({
 
   return (
     <div
-      className='flex flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
-      style={{ cursor: "pointer" }}
+      className='flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4'
+      style={{ maxWidth: "400px", cursor: "pointer" }}
       onClick={() => setTruncate(!truncateText)}
     >
       {book.cover_i && (
         <img
-          className='max-w-xs bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 cursor-pointer'
+          className='rounded-lg mb-4'
           src={`http://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
           alt={book.title}
+          style={{ height: "200px", objectFit: "cover" }}
         />
       )}
-      <div className='flex flex-col justify-between p-4 w-full'>
-        <div className='overflow-hidden'>
-          <h5 className='mb-2 text-2x1 font-bold tracking-tight text-gray-900 dark:text-white'>
-            {book.title}
-          </h5>
-          <div>
-            <span className='font-semibold'>Author key:</span> {book.author_key}
-          </div>
-          <div>
-            <span className='font-semibold'>Name: </span>
-            {book.author_name}
-          </div>
-          <div>
-            <span className='font-semibold'>Ebook: </span>
-            {book.ebook_access}
-          </div>
-          <div>
-            <span className='font-semibold'>First Publish: </span>
-            {book.first_publish_year}
-          </div>
-          <div className={truncateText ? "truncate" : ""}>
-            <span className='font-semibold'>First sentence: </span>
+      <div className='w-full'>
+        <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
+          {book.title}
+        </h5>
+        <p className='text-gray-700 dark:text-gray-400'>
+          <span className='font-semibold'>Author key:</span> {book.author_key}
+          <br />
+          <span className='font-semibold'>Name:</span> {book.author_name}
+          <br />
+          <span className='font-semibold'>Ebook:</span> {book.ebook_access}
+          <br />
+          <span className='font-semibold'>First Publish:</span>{" "}
+          {book.first_publish_year}
+          <br />
+          <div className={truncateText ? "truncate max-w-full" : ""}>
+            <span className='font-semibold'>First sentence:</span>{" "}
             {book.first_sentence}
           </div>
-        </div>
-        <div className='flex flex-col md:flex-row items-center mt-4'>
-          {!isAddedToFavorites ? (
-            <button
-              className='mt-2 md:mt-0 md:ml-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'
-              onClick={handleAddToFavorites}
-            >
-              Add to Favorites
-            </button>
-          ) : (
-            <span className='text-green-500 text-2xl'>✔️</span>
-          )}
-          <div className='mt-2 md:mt-0 md:ml-2'>
-            <span>Add to read books:</span>
-            {!isAddedtoReadBooks ? (
+        </p>
+        <div className='flex justify-between mt-4'>
+          <div>
+            {!isAddedToFavorites ? (
+              <button
+                className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'
+                onClick={handleAddToFavorites}
+              >
+                Add to Favorites
+              </button>
+            ) : (
+              <span className='text-green-500 text-2xl'>✔️</span>
+            )}
+          </div>
+          <div>
+            {!isAddedToReadBooks ? (
               <button
                 className='ml-2 bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-500'
                 onClick={handleAddToReadBooks}
               >
-                +
+                Add to Read Books
               </button>
             ) : (
               <span className='text-green-500 text-2xl ml-2'>✔️</span>
             )}
           </div>
         </div>
-        <form onSubmit={handleReviewSubmit} className='mt-4 w-full'>
+        <form onSubmit={handleReviewSubmit} className='mt-4'>
           <textarea
             className='w-full p-2 border border-gray-300 rounded'
             placeholder='Write your review here...'
@@ -116,7 +112,9 @@ const BookCardSearch: React.FC<BookCardSearchProps> = ({
         </form>
         <div className='mt-4'>
           {submittedReview.map((review, index) => (
-            <div key={index}>{review}</div>
+            <div key={index} className='bg-gray-100 p-2 rounded mt-2'>
+              {review}
+            </div>
           ))}
         </div>
       </div>
